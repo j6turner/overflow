857586d35e155079989969338950141092082b24
James Maynard <jameskirkmaynard@gmail.com>
2015-03-24 15:22:38 -0700

    Add user show.html with questions/responses lists.

---
+++ spec/features/show_user_page_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "how a user can get to their individual page" do
+  it "will take the user to their own page" do
+    user = FactoryGirl.create(:user)
+    sign_in(user)
+    click_on "My Profile"
+    expect(page).to have_content "profile page"
+  end
+end
